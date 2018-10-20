44617bdd8a628a254fbe183cf285fb1dcf607d77
Hakeem <hakeem.bisyir@gmail.com>
2018-10-20 16:01:15 -0400

    Organized constants

--- DrinkConstants.py
+++ DrinkConstants.py
@@ -1,11 +1,5 @@
-from kivy.uix.label import Label
 
-MOTOR_1 = 'None'
-MOTOR_2 = 'None'
-MOTOR_3 = 'None'
-MOTOR_4 = 'None'
-MOTOR_5 = 'None'
-MOTOR_6 = 'None'
+MOTOR_MAP = {1:11, 2:13, 3:15, 4:16, 5:18, 6:22}
 
 ALK_MENU = ['Rum', 'Tequila', 'Gin', 'Vodka', 'Whiskey', 'Scotch']
 MIXER_MENU = ['Coke', 'Diet Coke', 'Pepsi', 'Sprite', 'Orange Juice', 'Cranberry Juice', 'Ginger Ale', 'Margarita Mix']
@@ -13,11 +7,3 @@ MIXER_MENU = ['Coke', 'Diet Coke', 'Pepsi', 'Sprite', 'Orange Juice', 'Cranberry
 
 DRINKS_MENU = ALK_MENU+MIXER_MENU
 
-DrinkLabel_1 = Label(text='None')
-DrinkLabel_2 = Label(text='None')
-DrinkLabel_3 = Label(text='None')
-DrinkLabel_4 = Label(text='None')
-DrinkLabel_5 = Label(text='None')
-DrinkLabel_6 = Label(text='None')
-
-CURRENT_MOTOR = 0
